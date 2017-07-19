27430a7f29e897e967b491af31541d29dc33a121
mrunal-selokar <mrunal_selokar@outlook.com>
2017-07-19 14:35:15 +0530

    demo animated right header added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(function(){
-    $("#add").on("click",function() {
-	    var val = $("input").val();
+    $("#plus").on("click",function() {
+		var val = $("input").val();
+		val = val.toUpperCase();
 	    if (val != '') {
 	        var el = $("<li></li>").text(val);
 	        $(el).append("<button class='rem'>X</button>");
@@ -11,4 +12,7 @@ $(function(){
        	$(this).parent().remove();
     });
     });
+    var rcircle = $("#rcircle");
+    rcircle.animate({width:'+=100px',height:'+=100px',top:'+=100px',lineHeight: '+=100px',fontSize:'+=8px'},500);
+    rcircle.animate({width:'-=100px',height:'-=100px',top:'-=100px',lineHeight: '-=100px',fontSize:'-=8px'},500);
 });
